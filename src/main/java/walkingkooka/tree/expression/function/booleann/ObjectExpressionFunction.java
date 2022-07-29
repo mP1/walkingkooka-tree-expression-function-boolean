5d93699242ddae0b83a5cf43885d4b1b3b074536
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-07-29 20:14:41 +1000

    https://github.com/mP1/walkingkooka-tree/pull/579 ExpressionFunction implements HasOptionalName
    - https://github.com/mP1/walkingkooka-tree/pull/579
    - ExpressionFunction implements HasOptionalName

--- src/main/java/walkingkooka/tree/expression/function/booleann/ObjectExpressionFunction.java
+++ src/main/java/walkingkooka/tree/expression/function/booleann/ObjectExpressionFunction.java
@@ -23,6 +23,8 @@ import walkingkooka.tree.expression.ExpressionPurityContext;
 import walkingkooka.tree.expression.FunctionExpressionName;
 import walkingkooka.tree.expression.function.ExpressionFunction;
 
+import java.util.Optional;
+
 /**
  * Common abstract base class for all Object functions in this package.
  */
@@ -30,15 +32,17 @@ abstract class ObjectExpressionFunction<C extends ExpressionEvaluationContext> i
 
     ObjectExpressionFunction(final String name) {
         super();
-        this.name = FunctionExpressionName.with(name);
+        this.name = Optional.of(
+                FunctionExpressionName.with(name)
+        );
     }
 
     @Override
-    public final FunctionExpressionName name() {
-        return name;
+    public final Optional<FunctionExpressionName> name() {
+        return this.name;
     }
 
-    private final FunctionExpressionName name;
+    private final Optional<FunctionExpressionName> name;
 
     @Override
     public final Class<Object> returnType() {
@@ -52,6 +56,8 @@ abstract class ObjectExpressionFunction<C extends ExpressionEvaluationContext> i
 
     @Override
     public final String toString() {
-        return this.name().toString();
+        return this.name()
+                .get()
+                .toString();
     }
 }
